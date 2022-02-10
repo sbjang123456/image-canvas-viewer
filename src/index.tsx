eac5a6edc5b5113a01bee5b9d4a3e52f78dad116
JangSubin <sbjang123456@gmail.com>
2022-02-10 23:55:55 +0900

    [bundler] init

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import HelloWorld from './components/HelloWorld';
+
+// document.body.appendChild(
+//     Object.assign(document.createElement(`div`), { id: 'root' })
+// );
+// ReactDOM.render(<HelloWorld name="JangSu" />, document.getElementById('root'));
+
+export { default as HelloWorld } from './components/HelloWorld';
